bf83161339cf7c3af834050a65d846c824c6d52d
LotusDeath69 <66803893+LotusDeath69@users.noreply.github.com>
2021-03-30 14:54:38 -0400

    Update FKDR.py

--- FKDR.py
+++ FKDR.py
@@ -8,31 +8,33 @@ def uuid(ign):
 
 
 def fkdr(ign, key):
-    data = requests.get(f'https://api.hypixel.net/player?key='
-                        f'{key}&uuid={uuid(ign)}').json()
-    total_final_kills = data['player']['stats']['Bedwars']['final_kills_bedwars']
-    total_final_deaths = data['player']['stats']['Bedwars']['final_deaths_bedwars']
-    total_fkdr = total_final_kills / total_final_deaths
+    try:
+        data = requests.get(f'https://api.hypixel.net/player?key='
+                            f'{key}&uuid={uuid(ign)}').json()
+        total_final_kills = data['player']['stats']['Bedwars']['final_kills_bedwars']
+        total_final_deaths = data['player']['stats']['Bedwars']['final_deaths_bedwars']
+        total_fkdr = total_final_kills / total_final_deaths
 
-    solo_final_kills = data['player']['stats']['Bedwars']['eight_one_final_kills_bedwars']
-    solo_final_deaths = data['player']['stats']['Bedwars']['eight_one_final_deaths_bedwars']
-    solo_fkdr = solo_final_kills / solo_final_deaths
+        solo_final_kills = data['player']['stats']['Bedwars']['eight_one_final_kills_bedwars']
+        solo_final_deaths = data['player']['stats']['Bedwars']['eight_one_final_deaths_bedwars']
+        solo_fkdr = solo_final_kills / solo_final_deaths
 
-    doubles_final_kills = data['player']['stats']['Bedwars']['eight_two_final_kills_bedwars']
-    doubles_final_deaths = data['player']['stats']['Bedwars']['eight_two_final_deaths_bedwars']
-    doubles_fkdr = doubles_final_kills / doubles_final_deaths
+        doubles_final_kills = data['player']['stats']['Bedwars']['eight_two_final_kills_bedwars']
+        doubles_final_deaths = data['player']['stats']['Bedwars']['eight_two_final_deaths_bedwars']
+        doubles_fkdr = doubles_final_kills / doubles_final_deaths
 
-    triples_final_kills = data['player']['stats']['Bedwars']['four_three_final_kills_bedwars']
-    triples_final_deaths = data['player']['stats']['Bedwars']['four_three_final_deaths_bedwars']
-    triples_fkdr = triples_final_kills / triples_final_deaths
+        triples_final_kills = data['player']['stats']['Bedwars']['four_three_final_kills_bedwars']
+        triples_final_deaths = data['player']['stats']['Bedwars']['four_three_final_deaths_bedwars']
+        triples_fkdr = triples_final_kills / triples_final_deaths
 
-    fours_final_kills = data['player']['stats']['Bedwars']['four_four_final_kills_bedwars']
-    fours_final_deaths = data['player']['stats']['Bedwars']['four_four_final_deaths_bedwars']
-    fours_fkdr = fours_final_kills / fours_final_deaths
+        fours_final_kills = data['player']['stats']['Bedwars']['four_four_final_kills_bedwars']
+        fours_final_deaths = data['player']['stats']['Bedwars']['four_four_final_deaths_bedwars']
+        fours_fkdr = fours_final_kills / fours_final_deaths
 
-    return f'Overall: {round(total_fkdr, 2)} \n\nSolo: {round(solo_fkdr, 2)} \nDoubles: {round(doubles_fkdr, 2)} ' \
-           f'\n3s: {round(triples_fkdr, 2)} \n4s: {round(fours_fkdr, 2)}'
-           
+        return f'Overall: {round(total_fkdr, 2)} \nSolo: {round(solo_fkdr, 2)} \nDoubles: {round(doubles_fkdr, 2)} ' \
+               f'\n3s: {round(triples_fkdr, 2)} \n4s: {round(fours_fkdr, 2)}\n'
+    except KeyError:
+        return "Player haven't played bw yet????"
 
 
 print(fkdr('IGN_HERE', 'API_KEY_HERE'))
