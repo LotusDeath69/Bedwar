31fbac7bb53b5c0720ed325636a15f920f7dfbc7
LotusDeath69 <66803893+LotusDeath69@users.noreply.github.com>
2021-04-02 12:31:50 -0400

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,33 @@
+import requests
+api = 'API_KEY_HERE'
+
+
+def uuid(ign):
+    data = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{ign}').json()
+    return data['id']
+
+
+def stats(ign, key):
+    data = requests.get(f'https://api.hypixel.net/player?key={key}&uuid={uuid(ign)}').json()
+
+    winstreak = data['player']['stats']['Bedwars']['winstreak']
+    games_played = data['player']['stats']['Bedwars']['games_played_bedwars']
+    losses = data['player']['stats']['Bedwars']['losses_bedwars']
+    wins = data['player']['stats']['Bedwars']['wins_bedwars']
+    beds_broken = data['player']['stats']['Bedwars']['beds_broken_bedwars']
+
+    "#Like Catacombs levels, it's inaccurate"
+    experience = data['player']['stats']['Bedwars']['Experience']
+    total_xp_per_level = 5000
+    current_level = experience / total_xp_per_level
+
+    final_kills = data['player']['stats']['Bedwars']['final_kills_bedwars']
+    final_deaths = data['player']['stats']['Bedwars']['final_deaths_bedwars']
+
+    return f'Level: {round(current_level)}\nWins: {wins}\nLosses: {losses}\nGames Played: {games_played}\n\n' \
+           f'Final Kills: {final_kills}\nFinal Deaths: {final_deaths}\n' \
+           f'Overall FKDR: {round(final_kills/final_deaths)}\n' \
+           f'Win Streak: {winstreak}\nBeds Broken: {beds_broken}'
+
+
+print(stats('thatbananaking', api))
