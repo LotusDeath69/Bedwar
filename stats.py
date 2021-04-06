37659a228abf6a2f7ec6eda89b40a4de2c0456b2
LotusDeath69 <66803893+LotusDeath69@users.noreply.github.com>
2021-04-06 19:53:46 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -2,6 +2,10 @@ import requests
 api = 'API_KEY_HERE'
 
 
+def formatPercentage(x):
+    return "{:.0%}".format(x)
+
+
 def uuid(ign):
     data = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{ign}').json()
     return data['id']
@@ -15,7 +19,8 @@ def stats(ign, key):
     losses = data['player']['stats']['Bedwars']['losses_bedwars']
     wins = data['player']['stats']['Bedwars']['wins_bedwars']
     beds_broken = data['player']['stats']['Bedwars']['beds_broken_bedwars']
-
+    win_rate = round(wins / losses, 2)
+    
     "#Like Catacombs levels, it's inaccurate"
     experience = data['player']['stats']['Bedwars']['Experience']
     total_xp_per_level = 5000
@@ -24,7 +29,7 @@ def stats(ign, key):
     final_kills = data['player']['stats']['Bedwars']['final_kills_bedwars']
     final_deaths = data['player']['stats']['Bedwars']['final_deaths_bedwars']
 
-    return f'Level: {round(current_level)}\nWins: {wins}\nLosses: {losses}\nWin Rate: {round(wins/losses, 1)}\nGames Played: {games_played}\n\n' \
+    return f'Level: {round(current_level)}\nWins: {wins}\nLosses: {losses}\nWin Rate: {formatPercentage(win_rate)}\nGames Played: {games_played}\n\n' \
            f'Final Kills: {final_kills}\nFinal Deaths: {final_deaths}\n' \
            f'Overall FKDR: {round(final_kills/final_deaths)}\n' \
            f'Win Streak: {winstreak}\nBeds Broken: {beds_broken}'
